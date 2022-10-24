724eb216cd311b22249db43cfdd0cd67dc36dfb3
dann <danil.mekh@mail.ru>
2022-10-24 17:40:54 +0700

    отображение списков и детальной информации

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -14,7 +14,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href= "https://www.isbn-international.org/content/what-isbn"> ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
     def display_genre(self):
@@ -63,4 +64,5 @@ class Author(models.Model):
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        return '%s, %s' % (self.last_name, self.first_name)
+        return '{0}, {1}'.format(self.last_name, self.first_name)
+
--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,15 +1,59 @@
-# from django.shortcuts import render
-# from .models import Book, Author, BookInstance, Genre
-#
-#
-# def index(request):
-#     num_books = Book.objects.all().count()
-#     num_instances = BookInstance.objects.all().count()
-#     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-#     num_authors = Author.objects.count()
-#
-#     return render(
-#         request,
-#         'index.html', context=
-# {'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors},
-#     )
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, pk):
+        try:
+            book_id = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Book does not exist")
+
+        book_id=get_object_or_404(Book, pk=pk)
+
+        return render(
+            request,
+            'catalog/book_detail.html',
+            context={'book': book_id, }
+        )
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
